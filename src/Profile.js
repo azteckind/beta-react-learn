ea1559aeb97cfe203629a6270b6921b4810707e8
azteckind <73482202+azteckind@users.noreply.github.com>
2021-11-08 10:53:40 -0800

    feat: import/export component

---
+++ src/Profile.js
@@ -0,0 +1,11 @@
+export function Profile() {
+    return (
+      <img 
+        src="https://www.nasa.gov/sites/default/files/thumbnails/image/26646856911_ca242812ee_o_1.jpg"
+        alt="Katherine Johnson"
+        width="100"
+        height="100"
+      />
+    );
+}
+//When using an image, it's best to go directly to the source of the image rather than the Google search result
