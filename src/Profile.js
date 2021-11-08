e55c503ce3dfa726bb5a7a919f8e2c118e6468cf
azteckind <73482202+azteckind@users.noreply.github.com>
2021-11-08 14:53:05 -0800

    feat: passing props to a component - stopped at passing JSX as children

--- src/Profile.js
+++ src/Profile.js
@@ -1,11 +1,47 @@
+import { getImageUrl } from "./utils";
+
+function Avatar({person, size}) {
+    return(
+        <img 
+            className="avatar"
+            src={getImageUrl(person)}
+            alt={person.name}
+            width={size}
+            height={size}
+        />
+    )
+}
 export function Profile() {
     return (
-      <img 
-        src="https://www.nasa.gov/sites/default/files/thumbnails/image/26646856911_ca242812ee_o_1.jpg"
-        alt="Katherine Johnson"
-        width="100"
-        height="100"
-      />
+    //   <img 
+    //     src="https://www.nasa.gov/sites/default/files/thumbnails/image/26646856911_ca242812ee_o_1.jpg"
+    //     alt="Katherine Johnson"
+    //     width={100}
+    //     height={100}
+    //   />
+        <>
+            <Avatar
+                size={100}
+                person={{
+                    name: 'Katsuko Saruhashi',
+                    imageId: 'YfeOqp2'
+                }} 
+            />
+            <Avatar
+                size={80}
+                person={{
+                    name: 'Aklilu Lemma',
+                    imageId: 'OKS67lh'
+                }} 
+            />
+            <Avatar
+                size={50}
+                person={{
+                    name: 'Lin Lanying',
+                    imageId: '1bX5QH6'
+                }} 
+            />
+        </>
     );
 }
 //When using an image, it's best to go directly to the source of the image rather than the Google search result
